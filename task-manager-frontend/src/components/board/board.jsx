7d6a978bb056e9574d9b7484fc7ef4ae8daee77b
ponnanna-ks <ponnanna.ks@logycent.com>
2024-10-23 01:56:34 +0530

    add task pop up ui

--- task-manager-frontend/src/components/board/board.jsx
+++ task-manager-frontend/src/components/board/board.jsx
@@ -2,9 +2,20 @@ import { useEffect, useState } from 'react'
 import { getUserInfo } from '../../services/auth'
 import styles from './board.module.css'
 import { getTodaysDate } from '../../helper/utils'
+import { useNavigate } from 'react-router-dom'
+import { IoMdAdd } from "react-icons/io";
+import TaskPopUp from '../taskPopUp/taskPopUp'
 export default function Board ({}) {
     const [userData, setUserData] = useState({})
+    const [addtask, setAddtask] = useState(false)
 
+    const boardDivisions = [
+        {id: 0, name: 'Backlog'},
+        {id: 1, name: 'To Do'},
+        {id: 2, name: 'In Progress'},
+        {id: 3, name: 'Done'},
+    ]
+    const navigate = useNavigate()
     useEffect(() => {
         const getUserData = async() => {
             await getUserInfo().then((res) => {
@@ -25,17 +36,26 @@ export default function Board ({}) {
             </div>
             <div className={styles.boardHeader}>
                 <p className={styles.headerText}>Board</p>
-                <p>This week</p>
+                <p onClick={() => {setAddtask(true)}}>This week</p>
             </div>
             <div className={styles.tasksContainer}>
-                <div className={styles.taskCategory}></div>
-                <div className={styles.taskCategory}></div>
-                <div className={styles.taskCategory}></div>
-                <div className={styles.taskCategory}></div>
-                <div className={styles.taskCategory}></div>
-                <div className={styles.taskCategory}></div>
-                <div className={styles.taskCategory}></div>
+                {boardDivisions.map((item, index) => (
+                    <div key={index} className={styles.taskCategory}>
+                        <div className={styles.taskCategoryHeader}>
+                            <p>{item.name}</p>
+                            {item.id == 1 && <IoMdAdd onClick={() => {setAddtask(true)}} className={styles.addIcon}/>}
+                        </div>
+                    </div>
+                ))}
             </div>
+            <TaskPopUp 
+                isOpen={addtask}
+                onClose={() => {setAddtask(false)}}
+                onConfirm={() => {setAddtask(false)}}
+                message="Add Task"
+                cancelButtonText="Cancel"
+                confirmButtonText="Save"
+            />
         </div>
     )
 }
---
+++ task-manager-frontend/src/components/taskPopUp/taskPopUp.jsx
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+import styles from './taskPopUp.module.css';
+import { MdDelete } from "react-icons/md";
+
+export default function TaskPopUp({ isOpen, onClose}) {
+    if (!isOpen) return null;
+
+    const [formData, setFormData] = useState({
+        title: '',
+        priority: '',
+        assignTo: '',
+        checklist: [],
+        dueDate: ''
+    });
+
+    const [newChecklistItem, setNewChecklistItem] = useState('');
+    const [showChecklistInput, setShowChecklistInput] = useState(false);
+
+    const handleAddChecklistItem = () => {
+        setShowChecklistInput(true);
+        if (newChecklistItem.trim() !== '') {
+            setFormData({ ...formData, checklist: [...formData.checklist, newChecklistItem] });
+            setNewChecklistItem('');
+        }
+    };
+
+    const handleRemoveChecklistItem = (index) => {
+        const updatedChecklist = formData.checklist.filter((_, i) => i !== index);
+        setFormData({ ...formData, checklist: updatedChecklist });
+    };
+
+    const onSaveClick = () => {
+        console.log(formData)
+    }
+    
+    return (
+        <div className={styles.popupOverlay}>
+            <div className={styles.popupContent}>
+                <div className={styles.formGroupColumn}>
+                    <label className={styles.label}>Title&nbsp;<span style={{ color: 'red' }}>*</span></label>
+                    <input
+                        type="text"
+                        value={formData.title}
+                        onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+                        placeholder="Enter Task Title"
+                    />
+                </div>
+
+                <div className={styles.formGroupRow}>
+                    <label className={styles.label}>Select Priority&nbsp;<span style={{ color: 'red' }}>*</span></label>
+                    <div className={styles.radioGroup}>
+                        {['HIGH PRIORITY', 'MODERATE PRIORITY', 'LOW PRIORITY'].map((priority, index) => (
+                            <label key={index}>
+                                <input
+                                    type="radio"
+                                    value={priority}
+                                    checked={formData.priority === priority}
+                                    onChange={(e) => setFormData({ ...formData, priority: e.target.value })}
+                                />
+                                {priority}
+                            </label>
+                        ))}
+                    </div>
+                </div>
+
+                <div className={styles.formGroupRow}>
+                    <label className={styles.label}>Assign To&nbsp;</label>
+                    <input
+                        type="email"
+                        value={formData.assignTo}
+                        onChange={(e) => setFormData({ ...formData, assignTo: e.target.value })}
+                        placeholder="Add an assignee"
+                    />
+                </div>
+
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Checklist&nbsp;<span style={{ color: 'red' }}>*</span></label>
+                    <ul className={styles.checklist}>
+                        {formData.checklist.map((item, index) => (
+                            <li key={index}>
+                                <input type="checkbox" />
+                                {item}
+                                <MdDelete
+                                    className={styles.deleteButton}
+                                    onClick={() => handleRemoveChecklistItem(index)}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div className={styles.addChecklist}>
+                        {showChecklistInput && (
+                            <input
+                                type="text"
+                                value={newChecklistItem}
+                                onChange={(e) => setNewChecklistItem(e.target.value)}
+                                placeholder="Add New Item"
+                            />
+                        )}
+                        <button onClick={handleAddChecklistItem}>+ Add New</button>
+                    </div>
+                </div>
+
+                <div className={styles.popupButtons}>
+                    <div className={styles.formGroupRow}>
+                        <input
+                            type={"date"}
+                            value={formData.dueDate}
+                            onChange={(e) => setFormData({ ...formData, dueDate: e.target.value })}
+                            placeholder="Select due date"
+                            className={styles.dateInput}
+                        />
+                    </div>
+
+                    <button onClick={onClose} className={styles.cancelButton}>Cancel</button>
+                    <button onClick={onSaveClick} className={styles.saveButton}>Save</button>
+                </div>
+            </div>
+        </div>
+    );
+}
