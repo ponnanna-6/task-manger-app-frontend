44574177d9bf4b74832d2fd06804ea563300ff23
ponnanna-ks <ponnanna.ks@logycent.com>
2024-10-27 20:10:54 +0530

    checklist and state update indicator

--- task-manager-frontend/src/components/taskDisplayItem/taskDisplay.jsx
+++ task-manager-frontend/src/components/taskDisplayItem/taskDisplay.jsx
@@ -10,11 +10,13 @@ import { updateCheckListStatus, updateTaskState } from '../../services/tasks';
 import EmailIcon from '../emailIcon/emailIcon';
 import Popup from '../popup/popup';
 import { alertToast, errorToast } from '../../helper/toast';
+import ActivityIndicator from '../activityIndicator/activityIndicator';
 
 export function TaskDisplay({ task, onEditTask, deleteTask, setRefreshData, isPublic, collapse }) {
     const [showDropDown, setShowDropDown] = useState(false);
     const [showChecklist, setShowChecklist] = useState(false);
     const [deletePopup, setDeletePopup] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const dropdownRef = useRef(null);
     const buttonRef = useRef(null);
 
@@ -38,10 +40,13 @@ export function TaskDisplay({ task, onEditTask, deleteTask, setRefreshData, isPu
     }, [dropdownRef, buttonRef]);
 
     const updateState = async (state) => {
+        setIsLoading(true)
         const res = await updateTaskState(task._id, state)
         if (res.status == 200) {
+            setIsLoading(false)
             setRefreshData(true)
         } else {
+            setIsLoading(false)
             errorToast("Could not update task status")
         }
     }
@@ -102,11 +107,13 @@ export function TaskDisplay({ task, onEditTask, deleteTask, setRefreshData, isPu
         const updatedChecklist = task.checklist.map((item, i) =>
             i === index ? { ...item, checked: !item.checked } : item
         );
-
+        setIsLoading(true)
         const res = await updateCheckListStatus(task._id, updatedChecklist)
         if (res.status == 200) {
+            setIsLoading(false)
             setRefreshData(true)
         } else {
+            setIsLoading(false)
             errorToast("Could not update checklist")
         }
     };
@@ -124,95 +131,99 @@ export function TaskDisplay({ task, onEditTask, deleteTask, setRefreshData, isPu
         <div
             className={styles.taskContainer}
             style={isPublic ? { width: '100%', maxHeight: '80%', overflow: 'auto' } : {}}
-        >
-            <div className={styles.header1}>
-                <div className={styles.priorityContainer}>
-                    <p className={styles.priority}><span style={{ color: colors[task?.priority], fontSize: '2vw' }}>.</span>{task?.priority}</p>
+        >   
+            {isLoading && <ActivityIndicator />}
+            {!isLoading && <>
 
-                    {task?.assignedTo.length > 0 &&
-                        <div style={{ width: "1vw", height: "0.8vw", flexDirection: "row", display: "flex", gap: "0.2vw", borderRadius: "50%" }}>
-                            {task?.assignedTo.map((item, index) => (
-                                <EmailIcon email={item?.email} key={index} />
+                <div className={styles.header1}>
+                    <div className={styles.priorityContainer}>
+                        <p className={styles.priority}><span style={{ color: colors[task?.priority], fontSize: '2vw' }}>.</span>{task?.priority}</p>
+
+                        {task?.assignedTo.length > 0 &&
+                            <div style={{ width: "1vw", height: "0.8vw", flexDirection: "row", display: "flex", gap: "0.2vw", borderRadius: "50%" }}>
+                                {task?.assignedTo.map((item, index) => (
+                                    <EmailIcon email={item?.email} key={index} />
+                                ))}
+                            </div>
+                        }
+                    </div>
+
+                    {(showDropDown && !isPublic) && (
+                        <div ref={dropdownRef} className={styles.dropDown}>
+                            {Object.keys(dropDownOptions).map((option, index) => (
+                                <p key={index} className={styles.dropDownOptionText} style={{ cursor: 'pointer', color: option === "Delete" ? "red" : "black" }} onClick={dropDownOptions[option].onClick}>{option}</p>
+                            ))}
+                        </div>
+                    )}
+                    {!isPublic &&
+                        <button ref={buttonRef} style={{ border: 'none', backgroundColor: 'transparent', cursor: 'pointer' }}>
+                            <SlOptions
+                                className={styles.dropDownDots} onClick={() => setShowDropDown(!showDropDown)}
+                            />
+                        </button>
+                    }
+
+                </div>
+                <p
+                    className={styles.title}
+                >
+                    {task.title}
+                </p>
+
+                <div>
+                    <div className={styles.checklistHeaderRow}>
+                        <p className={styles.checklistName}>{`Checklist (${task?.checklist?.filter(item => item.checked).length}/${task.checklist.length})`}</p>
+                        {!isPublic && (
+                            showChecklist
+                                ? <IoIosArrowUp className={styles.arrowIcon} onClick={() => setShowChecklist(!showChecklist)} />
+                                : <IoIosArrowDown className={styles.arrowIcon} onClick={() => setShowChecklist(!showChecklist)} />
+                        )}
+                    </div>
+                    {(showChecklist || isPublic) &&
+                        <div className={styles.checklistDropContainer} style={isPublic ? { width: '100%' } : {}}>
+                            {task?.checklist?.map((item, index) => (
+                                <div key={index} className={styles.checklistItemContainer}>
+                                    <input
+                                        type="checkbox"
+                                        checked={item.checked}
+                                        onChange={() => !isPublic && toggleChecklistItem(index)}
+                                    />
+                                    <p>{item.message}</p>
+                                </div>
                             ))}
                         </div>
                     }
                 </div>
 
-                {(showDropDown && !isPublic) && (
-                    <div ref={dropdownRef} className={styles.dropDown}>
-                        {Object.keys(dropDownOptions).map((option, index) => (
-                            <p key={index} className={styles.dropDownOptionText} style={{ cursor: 'pointer', color: option === "Delete" ? "red" : "black" }} onClick={dropDownOptions[option].onClick}>{option}</p>
-                        ))}
-                    </div>
-                )}
-                {!isPublic &&
-                    <button ref={buttonRef} style={{ border: 'none', backgroundColor: 'transparent', cursor: 'pointer' }}>
-                        <SlOptions
-                            className={styles.dropDownDots} onClick={() => setShowDropDown(!showDropDown)}
-                        />
-                    </button>
-                }
-
-            </div>
-            <p
-                className={styles.title}
-            >
-                {task.title}
-            </p>
-
-            <div>
-                <div className={styles.checklistHeaderRow}>
-                    <p className={styles.checklistName}>{`Checklist (${task?.checklist?.filter(item => item.checked).length}/${task.checklist.length})`}</p>
+                <div className={styles.footer}>
+                    {task.dueDate &&
+                        <div className={styles.dueDate} style={{ backgroundColor: setDueDateColor(task?.dueDate) }}>
+                            <p>{getDueData(task?.dueDate)}</p>
+                        </div>
+                    }
                     {!isPublic && (
-                        showChecklist
-                            ? <IoIosArrowUp className={styles.arrowIcon} onClick={() => setShowChecklist(!showChecklist)} />
-                            : <IoIosArrowDown className={styles.arrowIcon} onClick={() => setShowChecklist(!showChecklist)} />
+                        <div className={styles.taskStatusDiv}>
+                            {taskStates.map((state, index) => (
+                                state !== task?.taskStatus ? (
+                                    <div className={styles.taskStatusItem}>
+                                        <p key={index} onClick={() => updateState(state)}>{state}</p>
+                                    </div>
+                                ) : null
+                            ))}
+                        </div>
                     )}
+
                 </div>
-                {(showChecklist || isPublic) &&
-                    <div className={styles.checklistDropContainer} style={isPublic ? { width: '100%' } : {}}>
-                        {task?.checklist?.map((item, index) => (
-                            <div key={index} className={styles.checklistItemContainer}>
-                                <input
-                                    type="checkbox"
-                                    checked={item.checked}
-                                    onChange={() => !isPublic && toggleChecklistItem(index)}
-                                />
-                                <p>{item.message}</p>
-                            </div>
-                        ))}
-                    </div>
-                }
-            </div>
 
-            <div className={styles.footer}>
-                {task.dueDate &&
-                    <div className={styles.dueDate} style={{ backgroundColor: setDueDateColor(task?.dueDate) }}>
-                        <p>{getDueData(task?.dueDate)}</p>
-                    </div>
-                }
-                {!isPublic && (
-                    <div className={styles.taskStatusDiv}>
-                        {taskStates.map((state, index) => (
-                            state !== task?.taskStatus ? (
-                                <div className={styles.taskStatusItem}>
-                                    <p key={index} onClick={() => updateState(state)}>{state}</p>
-                                </div>
-                            ) : null
-                        ))}
-                    </div>
-                )}
-
-            </div>
-
-            <Popup
-                onConfirm={deleteConfirm}
-                isOpen={deletePopup}
-                onClose={() => setDeletePopup(false)}
-                message="Are you sure you want to Delete Task?"
-                cancelButtonText={"Cancel"}
-                confirmButtonText={"Yes, Delete"}
-            />
+                <Popup
+                    onConfirm={deleteConfirm}
+                    isOpen={deletePopup}
+                    onClose={() => setDeletePopup(false)}
+                    message="Are you sure you want to Delete Task?"
+                    cancelButtonText={"Cancel"}
+                    confirmButtonText={"Yes, Delete"}
+                />
+            </>}
         </div>
     )
 }
